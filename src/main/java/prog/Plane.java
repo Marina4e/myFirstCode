530cc5bf2c16bfc310235600c73c66224538a19c
Marina Che <118602185+Marina4e@users.noreply.github.com>
2022-12-08 23:31:42 +0200

    my firth code

---
+++ src/main/java/prog/Plane.java
@@ -0,0 +1,34 @@
+package prog;
+
+public class Plane {
+    public static String modelName = "vipPlane";
+        private String color;
+        public int passengers;
+        private String[] passengerNames = new String[6];
+        public Plane(String c) {
+            color = c;}
+        public void paintPlane(String c) {
+            color = c;
+        }
+        public String getPlaneColor() {
+            return color;
+        }
+        public void  setPassenger(int passengers) {
+        this.passengers = passengers;
+    }
+    public int getPassenger() {
+        return passengers;
+    }
+    public String[] getPassengerNames() {
+        return passengerNames;
+    }
+    public String getPassenger (int index) {
+            return passengerNames[index];
+    }
+
+    public void setPassengerNames(String[] passengerNames) {
+            this.passengerNames = passengerNames;
+    }
+}
+
+
---
+++ src/main/java/prog/SomeStrings.java
@@ -0,0 +1,28 @@
+package prog;
+
+import java.util.Arrays;
+
+public class SomeStrings {
+    public static void main(String[] args) {
+        Plane somePlane = new Plane("yellow");
+        String[] passengers = new String[6];
+        passengers[0] = "Tesla";
+        passengers[1] = "Nik";
+        passengers[2] = "Alice";
+        passengers[3] = "Me";
+        passengers[4] = "Tom";
+        passengers[5] = "Jerry";
+        somePlane.setPassengerNames(passengers);
+        for (int i = 0; i < somePlane.getPassengerNames().length; i++) {
+            System.out.println( somePlane.getPassenger(i));
+        switch ( somePlane.getPassenger(i)){
+            case "Nik":
+                System.out.println("this is NiK!");
+        break;
+            default:
+                System.out.println("this is not Nik");}
+        }
+    }
+}
+
+
